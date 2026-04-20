251c645f98f93a1b0ba54f3cfa7812fa2458d486
Mark Moulder <mark.moulder@servicenow.com>
2026-04-20 14:08:12 -0600

    Update to fix firebase env. vars.

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getDatabase, ref, set, onValue, update } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAyqIL0oq_0PacB28-wm1b3Sb-9htPpTjU",
-  authDomain: "dda-team-building.firebaseapp.com",
-  databaseURL: "https://dda-team-building-default-rtdb.firebaseio.com/",
-  projectId: "dda-team-building",
-  storageBucket: "dda-team-building.firebasestorage.app",
-  messagingSenderId: "382483754461",
-  appId: "1:382483754461:web:1fee2791d71500f58d9a34"
+  apiKey:            import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain:        import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL:       import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId:         import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket:     import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId:             import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
